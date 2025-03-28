708c5670e3365f61854b7e7904364cc9ac6e9a79
tenohekabanzai <10ohekabanzai@gmail.com>
2025-03-28 14:18:46 +0530

    first

---
+++ src/app/layout.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { usePathname } from "next/navigation";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+
+export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>)
+{
+  const pathname = usePathname();
+  let layout:any
+  switch (pathname) {
+    case '/admin':
+      layout = (
+        <div className="admin-layout flex">
+          <aside>Admin Sidebar</aside>
+          <main>{children}</main>
+        </div>
+      );
+      break;
+
+    default:
+      layout = (
+        <div className="public-layout bg-emerald-500">
+          <nav>Public Navbar</nav>
+          <main>{children}</main>
+        </div>
+      );
+      break;
+  }
+
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      {layout} {/* ✅ Correctly render JSX instead of a string */}
+    </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-5xl font-bold">Hello World - '/' path</div>
+  );
+}
